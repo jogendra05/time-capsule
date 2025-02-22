9b21f7a4f28ae8defef6e091728cf547051c7b3c
yadavharsh2004 <harshy8013698@gmail.com>
2025-02-22 16:59:54 +0530

    Added Login Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,12 +5,15 @@ import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { Navbar } from "@/components/navbar";
+import { AuthProvider } from "@/hooks/use-auth";
 import NotFound from "@/pages/not-found";
 import Home from "@/pages/home";
 import Timeline from "@/pages/timeline";
 import Create from "@/pages/create";
 import About from "@/pages/about";
 import Gallery from "@/pages/gallery";
+import AuthPage from "@/pages/auth-page";
+
 
 function Router() {
   return (
@@ -20,6 +23,7 @@ function Router() {
       <Route path="/create" component={Create} />
       <Route path="/about" component={About} />
       <Route path="/gallery" component={Gallery} />
+      <Route path="/auth" component={AuthPage} />
       <Route component={NotFound} />
     </Switch>
   );
@@ -28,10 +32,12 @@ function Router() {
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="min-h-screen flex flex-col">
-        <Navbar />
-        <Router />
-      </div>
+      <AuthProvider>
+        <div className="min-h-screen flex flex-col">
+          <Navbar />
+          <Router />
+        </div>
+      </AuthProvider>
       <Toaster />
     </QueryClientProvider>
   );
