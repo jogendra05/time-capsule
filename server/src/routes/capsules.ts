e925ee75f5dded41c7a20648f6393721a06f8c1e
jogendra05 <jogendrasuthar03@gmail.com>
2025-05-07 23:33:00 +0530

    Setup routes

--- server/src/routes/capsules.ts
+++ server/src/routes/capsules.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express';
 import { authenticate } from '../middleware/auth';
+import { createCapsule, deleteCapsule, getCapsule, listCapsules, updateCapsule } from '../controllers/capsuleController';
 
 const router = Router();
 
 router.use(authenticate);
+router.post('/create', createCapsule);
+router.get('/list', listCapsules);
+router.get('/get/:id', getCapsule);
+router.put('/update/:id', updateCapsule);
+router.delete('/delete/:id',deleteCapsule);
 
 
 export default router;
